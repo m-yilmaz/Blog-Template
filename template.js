f7488f028e33a7d26008cb717922944aae734d54
MUSTAFA YILMAZ <meka.mustafayilmaz@outlook.com>
2022-05-01 12:57:09 +0300

    done

---
+++ template.js
@@ -0,0 +1,4 @@
+var sidebarContent = document.getElementById("sidebar-content");
+var offcanvasContent = document.getElementById("offcanvas-content");
+
+offcanvasContent.innerHTML = sidebarContent.innerHTML;
